6c62f9602005ec8c4d844f4bbe69d41624121542
toilacube <151115rubikcube@gmail.com>
2024-04-26 11:09:30 +0700

    enable cache in products listing make 10x faster response

---
+++ src/main/java/com/example/crud/Config/CacheConfig.java
@@ -0,0 +1,28 @@
+package com.example.crud.Config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.github.benmanes.caffeine.cache.Caffeine;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+    @Bean
+    public CacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("productCache"); // Define your cache names here
+        cacheManager.setCaffeine(caffeineCacheBuilder());
+        return cacheManager;
+    }
+
+    Caffeine<Object, Object> caffeineCacheBuilder() {
+        return Caffeine.newBuilder()
+                .initialCapacity(100)
+                .maximumSize(500)
+                .expireAfterWrite(Duration.ofDays(7));
+    }
+}
--- src/main/java/com/example/crud/Product/ProductService.java
+++ src/main/java/com/example/crud/Product/ProductService.java
@@ -6,6 +6,8 @@ import com.example.crud.Entity.ProductItem;
 import com.example.crud.ProductItem.ProductItemResponse;
 import com.example.crud.ProductItem.ProductItemService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -13,21 +15,32 @@ import org.springframework.stereotype.Service;
 import com.example.crud.Product.ProductRepository;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService implements IProductService{
     private final ProductRepository productRepository;
     private final ProductItemService productItemService;
+    private final CacheManager cacheManager;
+
     @Override
     public PaginationResponse getAllProducts(int page, int pageSize) {
+
+        Cache cache = cacheManager.getCache("productCache");
+        if(cache != null){
+            if(cache.get("products") != null){
+                Logger.getLogger(ProductService.class.getName()).info("Cache hit");
+                return (PaginationResponse) cache.get("products").get();
+            }
+        }
 
 
         Pageable paging = PageRequest.of( page, pageSize);
 
         Page <Product> products = productRepository.findAll(paging);
 
-        int totalPages = products.getTotalPages() - 1; // because page starts from 0
+        Logger.getLogger(ProductService.class.getName()).info("Database hit");
 
         List<ProductResponse> productResponses = ProductResponse.fromEntity(products.getContent());
 
@@ -36,14 +49,22 @@ public class ProductService implements IProductService{
             productResponse.setProductItemResponse(productItems);
         }
 
+        // return products with pagination
+        int totalPages = products.getTotalPages() - 1; // because page starts from 0
+
         PaginationResponse paginationResponse = PaginationResponse.builder()
                 .totalPages(totalPages)
                 .data(productResponses)
                 .build();
 
+        if(cache != null){
+            cache.put("products", paginationResponse);
+        }
+
         return paginationResponse;
 
     }
+
 
     @Override
     public List<ProductResponse> getProductsByCategory() {
